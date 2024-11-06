d7ea719bf76de483eba9183210c015c5ae97a8e2
jayanth <jayanthkamath3.com>
2024-11-06 16:53:32 +0530

    Intial commit

---
+++ intro.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+
+using namespace std;
+
+int main(){
+    
+    //cout<<"hello"<<endl<<"hi"<<endl;
+    //int myAge = 23;
+    //cout<<typeid(myAge).name()<<endl;return 0;
+
+    /*int x = 25.5, y = 45.66f, z=9;
+
+    cout<<x<<endl<<y<<endl<<z<<endl;
+    return 0;*/
+
+    int x =5;
+    int y =2;
+    float z =x/y;
+    cout<<z<<endl;
+}
