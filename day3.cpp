071b4a71d578dbfee1e20073f747230635e4b92c
jayanth <jayanthkamath3.com>
2024-11-09 15:57:07 +0530

    day4

---
+++ day3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=10;
+    
+    cout<<"before"<<++x<<endl;
+
+    cout<<"after"<<x<<endl;
+
+    cout<<!false<<endl;
+}
---
+++ day4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=8;
+
+    if(x>9) cout<<"S"<<endl;
+    else if (x >8) cout<<"A"<<endl;
+    else if (x>7) cout<<"B"<<endl;
+}
